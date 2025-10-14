79754b9dc4607a60d7794791c834707b7f90d249
boladale faboya <boladaleadepeju@gmail.com>
2025-10-14 15:24:11 +0100

    one file added

---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Testing login ', function() {
+    beforeEach(function() {
+        cy.visit('https://parabank.parasoft.com/parabank/index.htm')
+})
+
+it('user should be able to login with valid credentials', function() {
+    cy.get('[name="username"]').type('fabz')
+    cy.get('[type="password"]').type('Pass12.@')
+    cy.get('[class="button"]').eq(1).click()
+})
+
+it('should not be able to login with invalid credentials', function()  {
+    
+})
+}) 
